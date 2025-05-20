bb6390aa08a8660387a8d14c13d564cd0fbec4a5
aske-wengel-frost <askefrost26@gmail.com>
2025-05-20 12:09:56 +0200

    Added remaps for windowfocus.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,7 +4,10 @@ vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', { desc = 'Save' })
 
 vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
 vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
-
+vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true })
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Find word" })
