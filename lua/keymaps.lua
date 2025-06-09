5dd2ac323e29d61bebd8fa91a5f8f4337af34d4c
aske-wengel-frost <askefrost26@gmail.com>
2025-06-09 11:23:39 +0200

    Added requested command for using telescope in .configs folder and added better filemanger.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,14 +2,30 @@ vim.g.mapleader = ' '
 
 vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', { desc = 'Save' })
 
-vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
+vim.keymap.set('n', '<C-n>', '<cmd>Oil<CR>', { desc = "Open parent directory using oil." })
+-- vim.keymap.set('n', '<C-n>', function() require('oil').toggle_float() end, {desc = "Toggle oil"}) -- if you want floating window instead.
 vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true })
 vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true })
 vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true })
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
+
+-- Customm telescope keybinding for finding config files.
+vim.keymap.set('n', '<leader>fc', function()
+    require('telescope.builtin').find_files({
+        prompt_title = 'Find Config Files',
+        cwd = vim.fn.expand("~/.config"),
+        hidden = true,
+        follow = true,
+        no_ignore = true,
+        find_command = {
+            'fd', '--type', 'f', '--hidden', '--follow', '--exclude', '.git'
+        }
+    })
+end, { desc = "Find in ~/.config" })
+
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Find word" })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find buffer" })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "find help" })
