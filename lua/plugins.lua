9daf62816d58235eb9121c74bfcd0c2292243007
aske-wengel-frost <askefrost26@gmail.com>
2025-06-14 19:51:33 +0200

    Fixed poorly formattet code.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -112,5 +112,5 @@ require("lazy").setup({
         -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
         -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
         lazy = false,
-    }
+    },
 })
