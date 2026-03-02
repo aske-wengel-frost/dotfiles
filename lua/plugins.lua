4003eafc30ea3c6ab812fdfb087d8e1bb0e0c12f
aske-wengel-frost <askefrost26@gmail.com>
2026-03-02 09:11:30 +0100

    added lualine and changed back to moonfly

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -212,5 +212,17 @@ require("lazy").setup({
 			end,
 		},
 		{ "nyoom-engineering/oxocarbon.nvim" },
+		{
+			"nvim-lualine/lualine.nvim",
+			dependencies = { "nvim-tree/nvim-web-devicons" },
+            config = function ()
+                require("lualine").setup({
+                    options = {
+                        theme = "auto",
+                        icons_enabled = true,
+                    }
+                })
+            end
+		},
 	},
 })
