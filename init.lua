063c3b5460504a428c2190b4c0a2552bcdf16d04
aske-wengel-frost <askefrost26@gmail.com>
2026-06-10 13:06:42 +0200

    donno what i did

--- init.lua
+++ init.lua
@@ -21,17 +21,10 @@ vim.pack.add({
 	{ src = "https://github.com/akinsho/toggleterm.nvim" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/folke/trouble.nvim" },
-	{ src = "https://github.com/rose-pine/neovim" },
+	{ src = "https://github.com/oskarnurm/koda.nvim" }
 })
 
 require("trouble").setup()
-require("rose-pine").setup({
-	variant = "moon",
-	styles = {
-		italic = false
-	}
-}
-)
 require("mason").setup({
 	firewall = {
 		enabled = true
@@ -69,7 +62,7 @@ for _, server in ipairs(servers) do
 	vim.lsp.enable(server)
 end
 
-vim.cmd.colorscheme("rose-pine")
+vim.cmd.colorscheme("koda-moss")
 vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format)
 vim.keymap.set("n", "<C-n>", ":Oil<CR>")
 vim.keymap.set("n", "<leader>gi", vim.lsp.buf.definition)
